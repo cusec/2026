75df4c5a5f8f7d17606ab7480304cbe56995ce87
Shrey Bhatt <shreybhatt130@yahoo.com>
2025-06-15 12:22:04 -0400

    Update fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,16 @@
 import type { Metadata } from "next";
+import { Space_Grotesk, Jost } from "next/font/google";
 import "./globals.css";
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  variable: "--font-space-grotesk", // Define the CSS variable
+});
+
+const jost = Jost({
+  subsets: ["latin"],
+  variable: "--font-jost", // Define the CSS variable
+});
 
 export const metadata: Metadata = {
   title: "CUSEC 2026",
@@ -12,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${spaceGrotesk.variable} ${jost.variable}`}>
       <body className={`antialiased`}>{children}</body>
     </html>
   );
