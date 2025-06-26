7165eceb1df8d145c7d8c4f17f9b4f65ed3c7026
Shrey Bhatt <shreybhatt130@yahoo.com>
2025-06-26 17:52:03 -0400

    Demo login page for scavenger hunt

---
+++ src/app/scavenger/page.tsx
@@ -0,0 +1,44 @@
+import { auth0 } from "@/lib/auth0";
+
+export default async function ScavengerPage() {
+  const session = await auth0.getSession();
+
+  if (!session) {
+    return (
+      <main>
+        <div className="bg-[url('/images/splash.svg')] bg-cover bg-center h-screen flex items-center justify-center gap-4">
+          <a href="/auth/login?screen_hint=signup&returnTo=/scavenger">
+            <button className="px-6 py-2 rounded-lg bg-primary text-white font-semibold shadow hover:bg-primary/80 transition">
+              Sign up
+            </button>
+          </a>
+          <a href="/auth/login?returnTo=/scavenger">
+            <button className="px-6 py-2 rounded-lg bg-light-mode text-primary font-semibold shadow hover:bg-primary hover:text-white transition">
+              Log in
+            </button>
+          </a>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <div className="bg-[url('/images/splash.svg')] bg-cover bg-center h-screen flex items-center justify-center">
+        <div className="flex flex-col items-center justify-center gap-4 bg-light-mode dark:bg-dark-mode rounded-4xl shadow-lg p-8 w-3/4 sm:w-1/2 md:w-1/3 lg:w-1/4">
+          <h1 className="text-primary dark:text-light-mode">
+            Welcome, {session.user.name}!{" "}
+            {session.user["cusec/roles"]?.includes("Admin") ? `(Admin)` : ""}
+          </h1>
+          <a
+            href={`/auth/logout?returnTo=${process.env.NEXT_PUBLIC_URL}/scavenger`}
+          >
+            <button className="px-6 py-2 rounded-lg bg-light-mode text-primary font-semibold shadow hover:bg-primary hover:text-white transition">
+              Log Out
+            </button>
+          </a>
+          <p>{JSON.stringify(session.user, null, 2)}</p>
+        </div>
+      </div>
+    </main>
+  );
+}
