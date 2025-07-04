5df75330e6edba8f05780c4bff600be3d97b4878
Shrey Bhatt <shreybhatt130@yahoo.com>
2025-07-04 11:37:48 -0400

    Fix temp login UI

--- src/components/feature/scavengerLogin.tsx
+++ src/components/feature/scavengerLogin.tsx
@@ -10,7 +10,7 @@ const ScavengerLogin = async () => {
   if (!user) {
     return (
       <main>
-        <div className="flex items-center justify-center gap-4">
+        <div className="min-w-fit flex items-center justify-center gap-4">
           <a href="/auth/login?screen_hint=signup&returnTo=/scavenger">
             <button className="px-6 py-2 rounded-lg bg-primary text-white font-semibold shadow hover:bg-primary/80 transition">
               Sign up
@@ -30,7 +30,7 @@ const ScavengerLogin = async () => {
   return (
     <main>
       <div className="flex items-center justify-center">
-        <div className="flex flex-col items-center justify-center gap-4 bg-light-mode dark:bg-dark-mode rounded-4xl shadow-lg p-8 w-3/4 sm:w-1/2 md:w-1/3 lg:w-1/4">
+        <div className="min-w-fit flex flex-col items-center justify-center gap-4 bg-light-mode dark:bg-dark-mode rounded-4xl shadow-lg p-8 w-3/4 sm:w-1/2 md:w-1/3 lg:w-1/4">
           <h1 className="text-primary dark:text-light-mode">
             Welcome, {user.name}!{" "}
             {user["cusec/roles"]?.includes("Admin") ? `(Admin)` : ""}
