bfd5ee4291c0f41b90162308bbcd61e6997a6731
Shrey Bhatt <shreybhatt130@yahoo.com>
2025-07-16 19:59:31 -0400

    City scape fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,11 @@ export default function Home() {
   return (
     <div className="bg-linear-[90deg] from-primary from-0% to-sunset to-100% -z-20 bg-cover bg-center h-full w-screen">
       <Navbar />
-      <Particles />
+
       <div className="relative z-10">
+        <Particles />
         <ParallaxCityscape />
-        <main className="w-full h-[120vh] flex justify-center items-center bg-linear-[35deg] from-primary from-0% via-primary/100 via-55% to-accent to-120%">
+        <main className="w-full h-[100vh] flex justify-center items-center bg-linear-[35deg] from-primary from-0% via-primary/100 via-55% to-accent to-120%">
           <div className="absolute top-[25vh]">
             <Hero />
           </div>
--- src/components/feature/parallaxCityscape.tsx
+++ src/components/feature/parallaxCityscape.tsx
@@ -1,10 +1,21 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
+import { useTransform, useScroll, motion } from "framer-motion";
 
 const ParallaxCityscape: React.FC = () => {
   const [isMobile, setIsMobile] = useState(false);
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["center end", "end start"],
+  });
+  const b1 = useTransform(scrollYProgress, [0.33, 1], ["0%", "-100%"]);
+  const b2 = useTransform(scrollYProgress, [0.33, 1], ["0%", "-50%"]);
+  const b3 = useTransform(scrollYProgress, [0.33, 1], ["0%", "-30%"]);
+  const l = useTransform(scrollYProgress, [0.33, 1], ["0%", "-110%"]);
+  const w = useTransform(scrollYProgress, [0.33, 0.6], ["0%", "-70%"]);
 
   const path = isMobile
     ? "M -1 40 C 203 48 436 34 734 41 C 997 48 1067 46 1200 40 L 1200 200 L 0 200 Z"
@@ -15,6 +26,9 @@ const ParallaxCityscape: React.FC = () => {
       setIsMobile(window.innerWidth < 768); // 768px is typical tablet breakpoint
     };
     handleResize();
+    scrollYProgress.onChange((latest) => {
+      console.log("Scroll Progress:", latest);
+    });
 
     window.addEventListener("resize", handleResize);
     return () => {
@@ -23,13 +37,13 @@ const ParallaxCityscape: React.FC = () => {
   }, []);
 
   return (
-    <div className="relative z-30">
+    <div className="absolute w-full h-[100vh] z-10" ref={container}>
       {/* Wavy Ground/Base */}
-      <div className="absolute top-[76vh]">
+      <motion.div style={{ y: w }} className="absolute top-[76vh]">
         <svg
           viewBox="0 0 1200 200"
           preserveAspectRatio="none"
-          className="w-full h-[65vh]"
+          className="w-full h-[80vh]"
         >
           <defs>
             <linearGradient
@@ -45,49 +59,61 @@ const ParallaxCityscape: React.FC = () => {
           </defs>
           <path d={path} fill="url(#groundGradient)" />
         </svg>
-      </div>
+      </motion.div>
 
       {/* Cityscape */}
       <div className="absolute top-[70vh] xl:top-[45vh] w-full">
         {/* Building 1 (Background) */}
-        <div className="absolute left-0 top-[5vh] max-h-[70vh] w-[8vh] h-[20vh] xl:w-[20vh] xl:h-[50vh] -z-10 transition-transform duration-75 ease-out">
+        <motion.div
+          style={{ y: b1 }}
+          className="absolute left-0 top-[5vh] max-h-[70vh] w-[8vh] h-[20vh] xl:w-[20vh] xl:h-[50vh] -z-10 transition-transform duration-75 ease-out"
+        >
           <Image
             src="/images/building1.svg"
             alt="Building 1"
             className="object-contain"
             fill
           />
-        </div>
+        </motion.div>
 
         {/* Building 2 (Middle) */}
-        <div className="absolute top-[10vh] left-[6vh] xl:left-[15vh] max-h-[60vh] w-[6vh] h-[15vh] xl:w-[18vh] xl:h-[38vh] -z-10 transition-transform duration-75 ease-out">
+        <motion.div
+          style={{ y: b2 }}
+          className="absolute top-[10vh] left-[6vh] xl:left-[15vh] max-h-[60vh] w-[6vh] h-[15vh] xl:w-[18vh] xl:h-[38vh] -z-10 transition-transform duration-75 ease-out"
+        >
           <Image
             src="/images/building2.svg"
             alt="Building 2"
             className="object-contain"
             fill
           />
-        </div>
+        </motion.div>
 
         {/* Building 3 (Small) */}
-        <div className="absolute hidden xl:block top-[38vh] w-[20vh] h-[11vh] -z-10 transition-transform duration-75 ease-out">
+        <motion.div
+          style={{ y: b3 }}
+          className="absolute hidden xl:block top-[38vh] w-[20vh] h-[11vh] -z-10 transition-transform duration-75 ease-out"
+        >
           <Image
             src="/images/building3.svg"
             alt="Building 3"
             className="object-contain"
             fill
           />
-        </div>
+        </motion.div>
 
         {/* Lighthouse (Foreground) */}
-        <div className="absolute right-0 max-h-[70vh] w-[11vh] h-[25vh] xl:w-[24vh] xl:h-[55vh] -z-10 transition-transform duration-75 ease-out">
+        <motion.div
+          style={{ y: l }}
+          className="absolute top-[5vh] xl:top-0 right-0 max-h-[70vh] w-[11vh] h-[25vh] xl:w-[24vh] xl:h-[55vh] -z-10 transition-transform duration-75 ease-out"
+        >
           <Image
             src="/images/lighthouse.svg"
             alt="Lighthouse"
             className="object-contain"
             fill
           />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
