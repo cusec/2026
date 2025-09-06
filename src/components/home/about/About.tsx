05bb1543353beade1fb7776918edbe8aca8fef14
Shrey Bhatt <shreybhatt130@yahoo.com>
2025-09-06 19:00:55 -0400

    Fix spacing for about section

--- src/components/home/about/About.tsx
+++ src/components/home/about/About.tsx
@@ -5,7 +5,7 @@ const About: React.FC = () => {
     <div className="mt-[25vh] md:mt-[50vh]">
       <div
         id="ABOUT"
-        className="max-w-[1800px] w-full flex flex-col justify-center mx-auto"
+        className="max-w-[1800px]  w-full flex flex-col justify-center mx-auto"
       >
         <div className="w-full flex justify-center text-center mb-4">
           <h2 className="text-3xl md:text-5xl xl:text-6xl font-bold pb-4 border-b-1 text-light-mode">
@@ -14,7 +14,7 @@ const About: React.FC = () => {
         </div>
 
         <div className="w-full flex justify-center text-center">
-          <div className="mx-[8vw] mt-10 lg:max-w-[48vw] rounded-3xl shadow-2xl">
+          <div className="mx-[8vw] mt-10 lg:min-w-[560px] xl:min-w-[750px] lg:max-w-[48vw] rounded-3xl shadow-2xl">
             <div className="p-8 md:p-12 xl:p-16 text-light-mode bg-light-mode/5 border-4 border-white/10 rounded-4xl backdrop-blur-xs">
               <Stats />
               <p className="text-xl md:text-xl lg:text-2xl xl:text-3xl leading-relaxed">
