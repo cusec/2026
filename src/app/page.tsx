7fee177a09033c0b2481c95432ddd5e291b5722d
Shrey Bhatt <shreybhatt130@yahoo.com>
2025-09-06 19:31:20 -0400

    Update gradients to be darker; Update wave

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,43 +38,39 @@ export default function Home() {
 
   return (
     <div className="bg-linear-[180deg] from-sea from-0% via-sea via-20% to-dark-mode to-100% bg-cover bg-center h-full w-screen">
-      <div className="w-full h-full bg-linear-[45deg] from-accent/25 from-0% via-accent/5 via-10% to-dark-mode/0 to-100%">
-        {showSplash ? (
-          <LoadingScreen
-            onComplete={() => {
-              setShowSplash(false);
-              setHideContent(false);
-            }}
-          />
-        ) : (
-          <>
-            <div
-              className={`${hideContent ? "min-h-[100vh]" : "hidden"}`}
-            ></div>
-            <div className={`relative ${hideContent ? "hidden" : ""}`}>
-              <Particles
-                key={particlesKey}
-                desktopParticleCount={400}
-                sizeMultiplier={1.5}
-              />
-              <Navbar />
-              <SmoothFollower />
-              <OceanSurface />
-              <main className="w-full h-[95vh] flex justify-center items-center bg-linear-[35deg] from-secondary from-0% via-primary/100 via-55% to-accent to-140%">
-                <div className="absolute xs:top-[13vh] sm:top-[25vh] z-10">
-                  <Hero />
-                </div>
-              </main>
-              <About />
-              <Gallery />
-              <Sponsors />
-              <Pricing />
-              <FAQ />
-              <Footer />
-            </div>
-          </>
-        )}
-      </div>
+      {showSplash ? (
+        <LoadingScreen
+          onComplete={() => {
+            setShowSplash(false);
+            setHideContent(false);
+          }}
+        />
+      ) : (
+        <>
+          <div className={`${hideContent ? "min-h-[100vh]" : "hidden"}`}></div>
+          <div className={`relative ${hideContent ? "hidden" : ""}`}>
+            <Particles
+              key={particlesKey}
+              desktopParticleCount={400}
+              sizeMultiplier={1.5}
+            />
+            <Navbar />
+            <SmoothFollower />
+            <OceanSurface />
+            <main className="w-full h-[95vh] flex justify-center items-center bg-linear-[35deg] from-dark-mode/60 from-0% via-primary/100 via-35% to-accent to-140%">
+              <div className="absolute xs:top-[13vh] sm:top-[25vh] z-10">
+                <Hero />
+              </div>
+            </main>
+            <About />
+            <Gallery />
+            <Sponsors />
+            <Pricing />
+            <FAQ />
+            <Footer />
+          </div>
+        </>
+      )}
     </div>
   );
 }
