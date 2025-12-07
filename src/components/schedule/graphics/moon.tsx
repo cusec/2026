eaf92b2c52db6767b385465ca8dbb79413bde000
Shrey Bhatt <55145217+KaosElegent@users.noreply.github.com>
2025-12-07 18:01:37 -0500

    Update sun and moon

--- src/components/schedule/graphics/moon.tsx
+++ src/components/schedule/graphics/moon.tsx
@@ -4,9 +4,9 @@ export default function Moon() {
   return (
     <div className="hidden md:block relative top-0 left-0">
       <div
-        className="hidden md:block absolute -z-10 w-60 lg:w-70 h-60 lg:h-70 -left-30 lg:-left-50"
+        className="hidden md:block absolute -z-10 w-60 lg:w-70 h-60 lg:h-70 -left-30 lg:-left-50 opacity-85"
         style={{
-          top: "-425px",
+          top: "-375px",
           rotate: "275deg",
         }}
         title="Moon"
--- src/components/schedule/graphics/sun.tsx
+++ src/components/schedule/graphics/sun.tsx
@@ -4,7 +4,7 @@ export default function Sun() {
   return (
     <div className="hidden md:block relative top-0 right-0">
       <div
-        className="hidden md:block absolute -z-10 w-60 lg:w-70 h-60 lg:h-70"
+        className="hidden md:block absolute -z-10 w-60 lg:w-70 h-60 lg:h-70 opacity-85"
         style={{
           top: "140px",
           right: "-170px",
