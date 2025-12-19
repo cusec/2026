c60c14a988c4a9ed0e9b70a27d56f8437c8da8d5
Shrey Bhatt <55145217+KaosElegent@users.noreply.github.com>
2025-12-19 07:33:34 -0500

    Ensure that the new pages don't get an unusual side bar

--- src/app/speakers/page.tsx
+++ src/app/speakers/page.tsx
@@ -9,7 +9,7 @@ import {
 
 export default function SpeakersPage() {
   return (
-    <div className="relative bg-linear-[35deg] from-dark-mode from-0% via-primary via-55% to-accent to-140% bg-cover bg-center h-full">
+    <div className="relative bg-linear-[35deg] overflow-x-hidden overflow-y-hidden from-dark-mode from-0% via-primary via-55% to-accent to-140% bg-cover bg-center h-full">
       <div className="bg-linear-[15deg] from-accent/20 from-0% via-primary/0 via-55% to-accent/0 to-100%">
         <Particles />
         <Navbar />
--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -9,7 +9,7 @@ import {
 
 export default function TeamPage() {
   return (
-    <div className="relative overflow-x-hidden bg-linear-[35deg] from-dark-mode from-0% via-primary via-55% to-accent to-140% bg-cover bg-center h-full">
+    <div className="relative overflow-x-hidden overflow-y-hidden bg-linear-[35deg] from-dark-mode from-0% via-primary via-55% to-accent to-140% bg-cover bg-center h-full">
       <div className="bg-linear-[15deg] from-accent/20 from-0% via-primary/0 via-55% to-accent/0 to-100%">
         <Particles />
         <Navbar />
